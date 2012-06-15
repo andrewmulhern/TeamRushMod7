77787373684956b474f6fb8f407cefdc07889fa9
Andrew Mulhern <ajm329@drexel.edu>
2012-06-15 16:06:26 -0400

    Adding modifications per 7.3

--- index.php
+++ index.php
@@ -8,18 +8,18 @@
 
 
 
-    <div id="wrapper">
-        <div id="col1">
-            <iframe src="versusData.php" frameborder="0" scrolling="no" width="100%" height="2350px"></iframe>
+        <div id="wrapper">
+            <div id="col1">
+                <iframe src="versusData.php" frameborder="0" scrolling="no" width="100%" height="2350px"></iframe>
+            </div>
+            <div id="col2">
+                <iframe src="versusData.php?hashtag=obama" frameborder="0" scrolling="no"  width="100%" height="2350px"></iframe>
+            </div>
+            <div id="mid">
+                <p>VS.</p>
+            </div>
+            <div id="bottom"></div>
         </div>
-        <div id="col2">
-            <iframe src="versusData.php?hashtag=obama" frameborder="0" scrolling="no"  width="100%" height="2350px"></iframe>
-        </div>
-        <div id="mid">
-            <p>VS.</p>
-        </div>
-        <div id="bottom"></div>
-    </div>
     
 	</body>
 </html>
